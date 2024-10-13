042effbbebebc2ded88e239d19de63aa4c57ba26
umarprogrammer19 <umarprogrammer19666@gmail.com>
2024-10-13 20:53:06 +0500

    Added Env Variables

--- src/config/firebase/firebaseConfig.js
+++ src/config/firebase/firebaseConfig.js
@@ -2,13 +2,14 @@ import { initializeApp } from "firebase/app";
 
 // web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBtAjj90S3HVFhfj6m6hI5ul_lHfjC3Ct8",
-    authDomain: "react-todo-121.firebaseapp.com",
-    projectId: "react-todo-121",
-    storageBucket: "react-todo-121.appspot.com",
-    messagingSenderId: "506210755593",
-    appId: "1:506210755593:web:9819b814a03d5122b604d8"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID
 };
+
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
